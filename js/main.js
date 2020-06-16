d9b52447e8f679e07a68f92a2468af7ffc90c349
KseniaKrasilnikova <tksenia13@gmail.com>
2020-06-16 20:30:36 +0300

    8. Личный проект: больше деталей (часть 1) PR edits

--- js/main.js
+++ js/main.js
@@ -1,69 +1,78 @@
 // генерация случайных данных
+'use strict';
 
-var adType = ["palace", "flat", "house", "bungalo"];
-var adCheckType = ["12:00", "13:00", "14:00"];
-var adFeatures = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
-var adPhotos = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
+var adType = ['palace', 'flat', 'house', 'bungalo'];
+var adCheckType = ['12:00', '13:00', '14:00'];
+var adFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var adPhotos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
 
 var getAds = function (count) {
   var ads = [];
   for (var i = 1; i <= count; i++) {
-    ads.push(getAd(i))
+    ads.push(getAd(i));
   }
   return ads;
 };
 
 function getAd(number) {
   var location = {
-    "x": Math.floor(Math.random() * 631),
-    "y": Math.floor(Math.random() * 630) + 130
+    'x': getRandomInt(0, 631),
+    'y': getRandomInt(130, 630)
   };
   return {
-    "author": {
-      "avatar": `img/avatars/user0${number}.png`
+    'author': {
+      'avatar': 'img/avatars/user0' + number + '.png'
     },
-    "offer": {
-      "title": `Заголовок ${number}`,
-      "address": `${location.x}, ${location.y}`,
-      "price": Math.floor(Math.random() * 2000) + 400,
-      "type": adType[Math.floor(Math.random() * adType.length)],
-      "rooms": Math.floor(Math.random() * 6) + 1,
-      "guests": Math.floor(Math.random() * 20) + 1,
-      "checkin": adCheckType[Math.floor(Math.random() * adCheckType.length)],
-      "checkout": adCheckType[Math.floor(Math.random() * adCheckType.length)],
-      "features": adFeatures.filter(function () {
-        return Math.random() >= 0.5
-      }),
-      "description": `Описание ${number}`,
-      "photos": adPhotos.filter(function () {
-        return Math.random() >= 0.5
-      })
+    'offer': {
+      'title': 'Заголовок ' + number,
+      'address': location.x + ', ' + location.y,
+      'price': getRandomInt(400, 2000),
+      'type': getRandomArrayItem(adType),
+      'rooms': getRandomInt(1, 6),
+      'guests': getRandomInt(1, 20),
+      'checkin': getRandomArrayItem(adCheckType),
+      'checkout': getRandomArrayItem(adCheckType),
+      'features': getRandomSubarray(adFeatures),
+      'description': 'Описание ' + number,
+      'photos': getRandomSubarray(adPhotos)
     },
-    "location": {
-      "x": location.x,
-      "y": location.y
+    'location': {
+      'x': location.x,
+      'y': location.y
     }
   };
 }
 
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * max) + min;
+}
+
+function getRandomArrayItem(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function getRandomSubarray(array) {
+  return array.filter(function () {
+    return Math.random() >= 0.5;
+  });
+}
+
 /**/
 
-document.querySelector(".map").classList.remove("map--faded");
+document.querySelector('.map').classList.remove('map--faded');
 
 // функция создания DOM-элемента на основе JS-объекта
-var similar = document.querySelector(".map__pins");
-
-var pinTemplate = document.querySelector("#pin")
-  .content
-  .querySelector(".map__pin");
 
 var renderAd = function (ad) {
+  var pinTemplate = document.querySelector('#pin')
+    .content
+    .querySelector('.map__pin');
   var adElement = pinTemplate.cloneNode(true);
   var avatarElement = adElement.getElementsByTagName('img')[0];
 
-  adElement.style.left = `${ad.location.x - 25}px`;
-  adElement.style.top = `${ad.location.y - 70}px`;
+  adElement.style.left = ad.location.x - 25 + 'px';
+  adElement.style.top = ad.location.y - 70 + 'px';
   avatarElement.src = ad.author.avatar;
   avatarElement.alt = ad.offer.title;
 
@@ -77,7 +86,7 @@ var appendAdElements = function (ads) {
   for (var i = 0; i < ads.length; i++) {
     adFragment.appendChild(renderAd(ads[i]));
   }
-  similar.appendChild(adFragment);
+  document.querySelector('.map__pins').appendChild(adFragment);
 };
 
 appendAdElements(getAds(8));
