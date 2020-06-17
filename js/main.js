ee5f7d455adc4e7cf92bfaba1a21e9e790c9fdb4
KseniaKrasilnikova <tksenia13@gmail.com>
2020-06-17 22:35:41 +0300

    9. Личный проект: больше деталей (часть 2)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 // генерация случайных данных
 'use strict';
 
-var adTypes = ['palace', 'flat', 'house', 'bungalo'];
+var adTypes = {
+  'palace': 'Дворец',
+  'flat': 'Квартира',
+  'house': 'Дом',
+  'bungalo': 'Бунгало'
+};
 var adCheckTypes = ['12:00', '13:00', '14:00'];
 var adFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var adPhotos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
@@ -29,7 +34,7 @@ function getAd(number) {
       'title': 'Заголовок ' + number,
       'address': location.x + ', ' + location.y,
       'price': getRandomInt(400, 2000),
-      'type': getRandomArrayItem(adTypes),
+      'type': getRandomArrayItem(Object.keys(adTypes)),
       'rooms': getRandomInt(1, 6),
       'guests': getRandomInt(1, 20),
       'checkin': getRandomArrayItem(adCheckTypes),
@@ -65,7 +70,7 @@ document.querySelector('.map').classList.remove('map--faded');
 
 // функция создания DOM-элемента на основе JS-объекта
 
-var renderAd = function (ad) {
+var renderAdPin = function (ad) {
   var pinTemplate = document.querySelector('#pin')
     .content
     .querySelector('.map__pin');
@@ -80,14 +85,59 @@ var renderAd = function (ad) {
   return adElement;
 };
 
+var renderAdCard = function (ad) {
+  var cardTemplate = document.querySelector('#card')
+    .content
+    .querySelector('.map__card');
+  var adElement = cardTemplate.cloneNode(true);
+  var avatarElement = adElement.querySelector('.popup__avatar');
+  var adTitle = adElement.querySelector('.popup__title');
+  var adAddress = adElement.querySelector('.popup__text--address');
+  var adPrice = adElement.querySelector('.popup__text--price');
+  var adType = adElement.querySelector('.popup__type');
+  var adRoomsAndGuests = adElement.querySelector('.popup__text--capacity');
+  var adCheckTime = adElement.querySelector('.popup__text--time');
+  var adFeature = adElement.querySelector('.popup__features');
+  var adDescription = adElement.querySelector('.popup__description');
+
+  avatarElement.src = ad.author.avatar;
+  adTitle.textContent = ad.offer.title;
+  adAddress.textContent = ad.offer.address;
+  adPrice.textContent = ad.offer.price + '₽/ночь';
+  adType.textContent = adTypes[ad.offer.type];
+
+  adRoomsAndGuests.textContent = ad.offer.rooms + ' комнаты' + ' для ' + ad.offer.guests + ' гостей';
+  adCheckTime.textContent = 'Заезд после ' + ad.offer.checkout + ', выезд до ' + ad.offer.checkin;
+  adFeature.textContent = ad.offer.features;
+  adDescription.textContent = ad.offer.description;
+  renderAdPhotos(adElement, ad.offer.photos);
+
+  return adElement;
+};
+
+function renderAdPhotos(adElement, photos) {
+  var adPhotosElement = adElement.querySelector('.popup__photos ');
+  var adPhotoElement = adElement.querySelector('.popup__photo ');
+
+  photos.forEach(function (photo) {
+    var clone = adPhotoElement.cloneNode(true);
+    clone.src = photo;
+    adPhotosElement.appendChild(clone);
+  });
+  adPhotoElement.remove();
+}
+
 // функция заполнения блока DOM-элементами
 
 var appendAdElements = function (ads) {
-  var adFragment = document.createDocumentFragment();
+  var adFragmentPin = document.createDocumentFragment();
+  var adFragmentCard = document.createDocumentFragment();
   for (var i = 0; i < ads.length; i++) {
-    adFragment.appendChild(renderAd(ads[i]));
+    adFragmentPin.appendChild(renderAdPin(ads[i]));
+    adFragmentCard.appendChild(renderAdCard(ads[i]));
   }
-  document.querySelector('.map__pins').appendChild(adFragment);
+  document.querySelector('.map__pins').appendChild(adFragmentPin);
+  document.querySelector('.map').insertBefore(adFragmentCard, document.querySelector('.map__filters-container'));
 };
 
 appendAdElements(getAds(8));
