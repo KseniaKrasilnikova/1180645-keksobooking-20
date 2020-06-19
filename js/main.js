55472a867dd122cbfab565401a9208cbfae612cd
KseniaKrasilnikova <tksenia13@gmail.com>
2020-06-19 18:57:50 +0300

    9. Личный проект: больше деталей (часть 2) PR-1 edits

--- js/main.js
+++ js/main.js
@@ -93,27 +93,52 @@ var renderAdCard = function (ad) {
   var avatarElement = adElement.querySelector('.popup__avatar');
   var adTitle = adElement.querySelector('.popup__title');
   var adAddress = adElement.querySelector('.popup__text--address');
-  var adPrice = adElement.querySelector('.popup__text--price');
   var adType = adElement.querySelector('.popup__type');
-  var adRoomsAndGuests = adElement.querySelector('.popup__text--capacity');
-  var adCheckTime = adElement.querySelector('.popup__text--time');
   var adFeature = adElement.querySelector('.popup__features');
   var adDescription = adElement.querySelector('.popup__description');
 
   avatarElement.src = ad.author.avatar;
   adTitle.textContent = ad.offer.title;
   adAddress.textContent = ad.offer.address;
-  adPrice.textContent = ad.offer.price + '₽/ночь';
+  renderPrice(adElement, ad.offer.price);
   adType.textContent = adTypes[ad.offer.type];
-
-  adRoomsAndGuests.textContent = ad.offer.rooms + ' комнаты' + ' для ' + ad.offer.guests + ' гостей';
-  adCheckTime.textContent = 'Заезд после ' + ad.offer.checkout + ', выезд до ' + ad.offer.checkin;
+  renderRoomsAndGuests(adElement, ad.offer);
+  renderCheckTime(adElement, ad.offer);
   adFeature.textContent = ad.offer.features;
   adDescription.textContent = ad.offer.description;
   renderAdPhotos(adElement, ad.offer.photos);
 
   return adElement;
 };
+
+function renderPrice(adElement, price) {
+  var adPrice = adElement.querySelector('.popup__text--price');
+  adPrice.textContent = price + ' ₽/ночь';
+}
+
+function renderRoomsAndGuests(adElement, offer) {
+  var adRoomsAndGuests = adElement.querySelector('.popup__text--capacity');
+  var adRooms = offer.rooms + ' комнаты';
+  var adGuests = offer.guests + ' гостей';
+
+  if (offer.rooms === 1) {
+    adRooms = offer.rooms + ' комната';
+  }
+  if (offer.rooms >= 5) {
+    adRooms = offer.rooms + ' комнат';
+  }
+
+  if (offer.guests === 1) {
+    adGuests = offer.guests + ' гостя';
+  }
+
+  adRoomsAndGuests.textContent = adRooms + ' для ' + adGuests;
+}
+
+function renderCheckTime(adElement, offer) {
+  var adCheckTime = adElement.querySelector('.popup__text--time');
+  adCheckTime.textContent = 'Заезд после ' + offer.checkout + ', выезд до ' + offer.checkin;
+}
 
 function renderAdPhotos(adElement, photos) {
   var adPhotosElement = adElement.querySelector('.popup__photos ');
@@ -132,9 +157,9 @@ function renderAdPhotos(adElement, photos) {
 var appendAdElements = function (ads) {
   var adFragmentPin = document.createDocumentFragment();
   var adFragmentCard = document.createDocumentFragment();
+  adFragmentCard.appendChild(renderAdCard(ads[0]));
   for (var i = 0; i < ads.length; i++) {
     adFragmentPin.appendChild(renderAdPin(ads[i]));
-    adFragmentCard.appendChild(renderAdCard(ads[i]));
   }
   document.querySelector('.map__pins').appendChild(adFragmentPin);
   document.querySelector('.map').insertBefore(adFragmentCard, document.querySelector('.map__filters-container'));
