4fef703e5be441d92cfd4f4acb171c675f1d7317
KseniaKrasilnikova <tksenia13@gmail.com>
2020-06-29 19:16:01 +0300

    module5-task2 PR-1 edits (renaming), new module - map.js

--- js/main.js
+++ js/main.js
@@ -1,53 +1,12 @@
 'use strict';
-var PIN_TAIL = 22;
+
 var formElement = document.querySelector('.ad-form');
 
-// Неактивное состояние.
-function setDisabledAttributes(form, tagName, isDisable) {
-  var elements = form.getElementsByTagName(tagName);
-  for (var i = 0; i < elements.length; i++) {
-    if (isDisable) {
-      elements[i].setAttribute('disabled', 'true');
-    } else {
-      elements[i].removeAttribute('disabled');
-    }
-  }
-}
-
-// Активация страницы ЛКМ, Enter
-var mapPinMainElement = document.querySelector('.map__pin--main');
-var activatePage = function (evt) {
-  if (evt.button !== 0 && evt.key !== 'Enter') {
-    return;
-  }
-  if (!isActivated()) {
-    return;
-  }
-  document.querySelector('.map').classList.remove('map--faded');
-  document.querySelector('.ad-form').classList.remove('ad-form--disabled');
-  setDisabledAttributes(formElement, 'input', false);
-  setDisabledAttributes(formElement, 'select', false);
-  window.adElements.appendPins(window.data.getAds(8));
-  setAddress();
-};
-
-function isActivated() {
-  return document.querySelector('.map--faded') !== null;
-}
-
-function setAddress() {
-  var yOffset = isActivated() ? Math.floor(mapPinMainElement.offsetHeight / 2) : mapPinMainElement.offsetHeight + PIN_TAIL;
-  var x = parseInt(mapPinMainElement.style.left, 10) + Math.floor(mapPinMainElement.offsetWidth / 2);
-  var y = parseInt(mapPinMainElement.style.top, 10) + yOffset;
-  formElement.querySelector('#address').value = x + ', ' + y;
-}
-
-// Запуск
-setDisabledAttributes(formElement, 'input', true);
-setDisabledAttributes(formElement, 'select', true);
+window.util.setDisabledAttributes(formElement, 'input', true);
+window.util.setDisabledAttributes(formElement, 'select', true);
 window.form.disableSelectOptions(window.form.guestsNumber, window.form.mapRooms['1']);
 
-setAddress();
+window.map.setAddress();
 
-mapPinMainElement.addEventListener('mousedown', activatePage, true);
-document.addEventListener('keydown', activatePage, true);
+window.map.mapPinMainElement.addEventListener('mousedown', window.map.activatePage, true);
+document.addEventListener('keydown', window.map.activatePage, true);
--- js/util.js
+++ js/util.js
@@ -15,9 +15,22 @@
     });
   }
 
+  // Неактивное состояние
+  function setDisabledAttributes(form, tagName, isDisable) {
+    var elements = form.getElementsByTagName(tagName);
+    for (var i = 0; i < elements.length; i++) {
+      if (isDisable) {
+        elements[i].setAttribute('disabled', 'true');
+      } else {
+        elements[i].removeAttribute('disabled');
+      }
+    }
+  }
+
   window.util = {
     getRandomInt: getRandomInt,
     getRandomArrayItem: getRandomArrayItem,
-    getRandomSubarray: getRandomSubarray
+    getRandomSubarray: getRandomSubarray,
+    setDisabledAttributes: setDisabledAttributes
   };
 })();
