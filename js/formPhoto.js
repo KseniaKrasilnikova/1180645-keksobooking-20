b4915484a92f324e49b1802c1e41a8f61295b702
KseniaKrasilnikova <tksenia13@gmail.com>
2020-07-11 21:41:10 +0300

    module8-task2

---
+++ js/formPhoto.js
@@ -0,0 +1,51 @@
+'use strict';
+
+(function () {
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+  var avatarFileChooser = document.querySelector('.ad-form__field input[type=file]');
+  var formFileChooser = document.querySelector('.ad-form__upload input[type=file]');
+  var avatarPreview = document.querySelector('.ad-form-header__preview img');
+
+  var formNewElement = document.createElement('img');
+  formNewElement.width = 70;
+  formNewElement.height = 70;
+
+  var formPreviewPlace = document.querySelector('.ad-form__photo');
+
+  avatarFileChooser.addEventListener('change', function () {
+    var file = avatarFileChooser.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        avatarPreview.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+
+  formFileChooser.addEventListener('change', function () {
+    var file = formFileChooser.files[0];
+    var fileName = file.name.toLowerCase();
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+      reader.addEventListener('load', function () {
+        formPreviewPlace.insertAdjacentElement('afterbegin', formNewElement);
+        formNewElement.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+})();
